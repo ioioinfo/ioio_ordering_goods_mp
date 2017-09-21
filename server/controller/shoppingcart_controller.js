abe1da1dc2f922828c7bd38cce5afecab8e7e00c
apple2@ioioinfo.com <apple2@ioioinfo.com>
2017-09-21 19:56:22 +0800

    update

--- server/controller/shoppingcart_controller.js
+++ server/controller/shoppingcart_controller.js
@@ -42,7 +42,33 @@ exports.register = function(server, options, next) {
                 var cart_code = "";
                 api.sarch_cart_infos(person_id,cart_code,function(err,rows){
                     if (!err) {
-                        return reply({"success":true,"rows":rows.shopping_carts,"products":rows.products});
+                        return reply({"success":true,"rows":rows.shopping_carts,"products":rows.products,"total_data":rows.total_data});
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+        //删除购物车
+        {
+            method: 'POST',
+            path: '/delete_shopping_carts',
+            handler: function(request, reply) {
+                var person_id = "2c293d70-4506-11e7-ad37-e93548b3e6bc";
+                var cart_code = "";
+                var id = request.payload.id;
+                var ids = [];
+				ids.push(id);
+				ids = JSON.stringify(ids);
+                api.delete_shopping_carts(ids,function(err,rows){
+                    if (!err) {
+                        api.sarch_cart_infos(person_id,cart_code,function(err,rows){
+                            if (!err) {
+                                return reply({"success":true,"rows":rows.shopping_carts,"products":rows.products,"total_data":rows.total_data});
+                            }else {
+                                return reply({"success":false,"message":rows.message});
+                            }
+                        });
                     }else {
                         return reply({"success":false,"message":rows.message});
                     }
--- server/services/shoppingcart_api.js
+++ server/services/shoppingcart_api.js
@@ -22,6 +22,17 @@ var host = "http://211.149.248.241:18015/";
 
 var nav = function(server) {
     return {
+        //删除购物车
+        delete_shopping_carts: function(ids, cb) {
+            var url = host + "delete_shopping_carts?ids="+ids;
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,JSON.parse(body));
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
         //查询有人购物车
         sarch_cart_infos: function(person_id,cart_code, cb) {
             var url = host + "find_person_cart?person_id="+person_id+"&cart_code="+cart_code;
